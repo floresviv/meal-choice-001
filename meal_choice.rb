15c9070371cead035d90fc291a937f902dd48b6e
github username <github email address>
2015-10-02 12:40:35 -0400

    done with my meal

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,2 +1,6 @@
 # Define a method meal_choice that returns the meal_choice that was
+def meal_choice(choice = "meat")
+	puts "I choose #{choice} for my meal."
+	choice
+end
 # passed into it and defaults to meat.
